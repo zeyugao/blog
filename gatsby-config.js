e7eb70ad5dd706e18761b936d9673c4dc37c75bd
Elsa <zeyugao@outlook.com>
2021-04-07 22:28:15 +0800

    Update description and url

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
   siteMetadata: {
     title: `Blog by Elsa`,
     name: `Elsa Granger`,
-    siteUrl: `https://elsagranger.com`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    siteUrl: `https://blog.elsagranger.com`,
+    description: `Elsa Granger's Blog`,
 
     // important to set the main text that appears in the hero
     hero: {
